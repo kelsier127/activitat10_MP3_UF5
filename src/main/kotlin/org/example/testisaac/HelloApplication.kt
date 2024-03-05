fde72fa35274e2143c067938e53f376be31e15e4
hugoalonso7e5 <hugo.alonso.7e5@itb.cat>
2024-03-05 10:46:13 +0100

    Funciones "creadas"

--- src/main/kotlin/org/example/testisaac/HelloApplication.kt
+++ src/main/kotlin/org/example/testisaac/HelloApplication.kt
@@ -3,17 +3,23 @@ package org.example.testisaac
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.stage.Stage
 
 class HelloApplication : Application() {
+
+
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
+        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("preguntas.fxml"))
+        val scene = Scene(fxmlLoader.load(), 700.0, 700.0)
+
         stage.title = "Hello!"
         stage.scene = scene
         stage.show()
     }
 }
+
 
 fun main() {
     Application.launch(HelloApplication::class.java)
--- src/main/kotlin/org/example/testisaac/HelloController.kt
+++ src/main/kotlin/org/example/testisaac/HelloController.kt
@@ -1,14 +1,42 @@
 package org.example.testisaac
 
 import javafx.fxml.FXML
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 
 class HelloController {
     @FXML
-    private lateinit var welcomeText: Label
+    private lateinit var btnQ1: Button
+    @FXML
+    private lateinit var btnQ2: Button
+    @FXML
+    private lateinit var btnQ3: Button
+    @FXML
+    private lateinit var btnQ4: Button
+    @FXML
+    private lateinit var btnQ5: Button
+    @FXML
+    private lateinit var btnQ6: Button
+    @FXML
+    private lateinit var labelID:Label
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private fun funcionPregunta1() {
+       labelID.text = "Este es el boton de pregunta 1"
+    }@FXML
+    private fun funcionPregunta2() {
+       labelID.text = "Este es el boton de pregunta 2"
+    }@FXML
+    private fun funcionPregunta3() {
+       labelID.text = "Este es el boton de pregunta 3"
+    }@FXML
+    private fun funcionPregunta4() {
+       labelID.text = "Este es el boton de pregunta 4"
+    }@FXML
+    private fun funcionPregunta5() {
+       labelID.text = "Este es el boton de pregunta 5"
+    }@FXML
+    private fun funcionPregunta6() {
+       labelID.text = "Este es el boton de pregunta 6"
     }
 }
