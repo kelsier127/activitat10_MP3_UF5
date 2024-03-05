f41bb13edc2dcf3e3f710f004c2da9e3055398f9
Max Dron <maxdron@icloud.com>
2024-03-05 20:47:44 +0100

    Se añade variable preguntaActual para poder cambiar de una otra; Se actualizan las funciones funcionPregunta; Se modifica la logica de la funcion guardarRespuesta; Se añade funcion mostrarPregunta; Se añade funcion salirDeLaAplicacion;

--- src/main/kotlin/org/example/testisaac/HelloController.kt
+++ src/main/kotlin/org/example/testisaac/HelloController.kt
@@ -1,16 +1,13 @@
 package org.example.testisaac
 
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.scene.control.*
 
 class HelloController {
 
-    //TODO
-    // - Guardar la respuesta de cada pregunta
-    // - Condicionales para escoger el personaje a partir de las respuestas
-    // - Enseñar el personaje
-    // - Cambio de escenas?
 
+    private var preguntaActual: Int = -1  // Añade esta variable para llevar el control de la pregunta actual
 
     //Variables del xml
     lateinit var btnAnswer: Button
@@ -39,7 +36,9 @@ class HelloController {
     // Grupo para escoger solamente una opcion
     private val toggleGroup = ToggleGroup()
 
-
+    // Añadir un sistema para rastrear las preguntas respondidas
+    // 6 preguntas en total, inicialmente todas en 'false'
+    private val preguntasRespondidas = BooleanArray(6) { false }
 
     @FXML
     /**
@@ -48,7 +47,10 @@ class HelloController {
      * El cambio en el label.text hace que cambie la pregunta tambien.
      */
     private fun funcionPregunta1() {
-        val answerList = listOf("1respuesta1","1respuesta2","1respuesta3","1respuesta4")
+        val answerList = listOf("Me han abandonado",
+            "Con mama o papa, se divorciaron por mi culpa.",
+            "Solo, sin confianza no hay traición.",
+            "Con quien se deje.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true //cambio de todos los radioButton a visible
@@ -56,7 +58,9 @@ class HelloController {
             list[i].text = answerList[i] //cambio de la pregunta de cada radioButton
         }
 
-        labelID.text = "Con quien vives?"
+        labelID.text = "¿Con quien vives?"
+        preguntaActual = 0
+
 
     }
     /**
@@ -66,14 +70,19 @@ class HelloController {
      */
     @FXML
     private fun funcionPregunta2() {
-        val answerList = listOf("2respuesta1","2respuesta2","2respuesta3","2respuesta4")
+        val answerList = listOf("Yo solo quiero que me quieran.",
+            "Que a mi madre se le rompa el cinturón de los castigos.",
+            "Conseguiré lo que sea como sea.",
+            "Quiero lo tuyo y quiero lo mio.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true
             list[i].toggleGroup = toggleGroup
             list[i].text = answerList[i]
         }
-        labelID.text = "Pregunta2"
+        labelID.text = "¿Qué es lo que más quieres en el mundo?"
+        preguntaActual = 1
+
     }
     /**
      * Funcion que se ejecuta al presionar le boton de Pregunta 3
@@ -82,14 +91,19 @@ class HelloController {
      */
     @FXML
     private fun funcionPregunta3() {
-        val answerList = listOf("3respuesta1","3respuesta2","3respuesta3","3respuesta4")
+        val answerList = listOf("Estoy más solo que un pingüino en el desierto",
+            "Los golpes de mi mamá cuentan?",
+            "Lo estoy intentando con varias, a ver qué consigo.",
+            "Solo quiero quedarme con todo lo de alguien.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true
             list[i].toggleGroup = toggleGroup
             list[i].text = answerList[i]
         }
-        labelID.text = "Pregunta3"
+        labelID.text = "¿Cómo es tu vida morosa?"
+        preguntaActual = 2
+
     }
     /**
      * Funcion que se ejecuta al presionar le boton de Pregunta 4
@@ -98,14 +112,19 @@ class HelloController {
      */
     @FXML
     private fun funcionPregunta4() {
-        val answerList = listOf("4respuesta1","4respuesta2","4respuesta3","4respuesta4")
+        val answerList = listOf("Encontrar a alguien que me quiera.",
+            "Que la gente de mi alrededor me trate bien.",
+            "La traición",
+            "El poder del capitalismo.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true
             list[i].toggleGroup = toggleGroup
             list[i].text = answerList[i]
         }
-        labelID.text = "Pregunta4"
+        labelID.text = "¿Qué te motiva?"
+        preguntaActual = 3
+
     }
     /**
      * Funcion que se ejecuta al presionar le boton de Pregunta 5
@@ -114,14 +133,19 @@ class HelloController {
      */
     @FXML
     private fun funcionPregunta5() {
-        val answerList = listOf("5respuesta1","5respuesta2","5respuesta3","5respuesta4")
+        val answerList = listOf("A nadie le importa si fallo.",
+            "Mi madre me hace lidiar con el.",
+            "Hago fracasar a los demás.",
+            "Yo no fracaso.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true
             list[i].toggleGroup = toggleGroup
             list[i].text = answerList[i]
         }
-        labelID.text = "Pregunta5"
+        labelID.text = "¿Cómo lidias con el fracaso?"
+        preguntaActual = 4
+
     }
     /**
      * Funcion que se ejecuta al presionar le boton de Pregunta 6
@@ -130,7 +154,10 @@ class HelloController {
      */
     @FXML
     private fun funcionPregunta6() {
-        val answerList = listOf("6respuesta1","6respuesta2","6respuesta3","6respuesta4")
+        val answerList = listOf("No quiero molestar, no le diré nada a nadie.",
+            "Avisaria a la persona y se lo daría.",
+            "No creo que lo necesite.",
+            "Lo que es tuyo es mio y lo que es mio también.")
         val list = listOf(lostRadioButton,isaacRadioButton,judasRadioButton,keeperRadioButton)
         for (i in list.indices) {
             list[i].isVisible = true
@@ -138,25 +165,96 @@ class HelloController {
             list[i].text = answerList[i]
             list[i].isSelected = false
         }
-        labelID.text = "Pregunta6"
+        labelID.text = "¿Qué harias si a alguien se le cae un billete y no se da cuenta?"
+        preguntaActual = 5
+
     }
 
     //Cuando pulsas el boton para guardar respuesta, esta es la funcion que se ejecuta
     @FXML
     private fun guardarResposta() {
-        // Verificar qué RadioButton está seleccionado y actualizar la cuenta correspondiente
-        val selectedRadioButton = toggleGroup.selectedToggle as RadioButton?
-        when (selectedRadioButton) {
-            lostRadioButton -> lost++
-            isaacRadioButton -> isaac++
-            judasRadioButton -> judas++
-            keeperRadioButton -> keeper++
+
+        // Obtener el RadioButton seleccionado
+        val selectedRadioButton = toggleGroup.selectedToggle as? RadioButton
+
+        // Incrementar la variable correspondiente basándose en el índice del RadioButton seleccionado
+        when (toggleGroup.toggles.indexOf(selectedRadioButton)) {
+            0 -> lost++
+            1 -> isaac++
+            2 -> judas++
+            3 -> keeper++
+        }
+
+        // Determinar cuál pregunta fue respondida basándose en el botón de pregunta activo
+        val preguntaRespondidaIndex = when {
+            btnQ1.isFocused -> 0
+            btnQ2.isFocused -> 1
+            btnQ3.isFocused -> 2
+            btnQ4.isFocused -> 3
+            btnQ5.isFocused -> 4
+            btnQ6.isFocused -> 5
+            else -> null
+        }
+        // Marcar la pregunta actual como respondida
+        /*if (preguntaRespondidaIndex != null) {
+            preguntasRespondidas[preguntaRespondidaIndex] = true
+        }*/
+        if (preguntaActual in preguntasRespondidas.indices) {
+            preguntasRespondidas[preguntaActual] = true
+        }
+
+        // Verificar si todas las preguntas han sido respondidas
+        val todasRespondidas = preguntasRespondidas.all { it }
+
+        if (todasRespondidas) {
+            // Si todas las preguntas han sido respondidas, determinar el personaje con la mayor puntuación
+            val maxPuntuacion = maxOf(lost, isaac, judas, keeper)
+            val mensajePersonaje = when (maxPuntuacion) {
+                lost -> "Eres Lost"
+                isaac -> "Eres Isaac"
+                judas -> "Eres Judas"
+                keeper -> "Eres Keeper"
+                else -> "Error" // Se agrega un mensaje de error por defecto
+            }
+
+            // Mostrar el mensaje final
+            labelID.text = "$mensajePersonaje. Test finalizado. Gracias por participar."
+
+            // Mostrar el mensaje final
+            labelID.text = "$mensajePersonaje. Test finalizado. Gracias por participar."
+
+            // Cambiar texto del botón a "Salir" y actualizar la acción para cerrar la aplicación
+            btnAnswer.text = "Salir"
+            btnAnswer.setOnAction { salirDeLaAplicacion() }
+
+        } else {
+            // Pasar a la siguiente pregunta no respondida
+            if (preguntaActual + 1 in preguntasRespondidas.indices) {
+                preguntaActual += 1
+                mostrarPregunta(preguntaActual)
+            }
         }
 
         // Reiniciar la selección para la siguiente pregunta
         toggleGroup.selectedToggle?.isSelected = false
 
-        // Actualizar mensaje en la interfaz para informar al usuario
-        labelID.text = "Respuesta guardada. Selecciona otra pregunta."
+
+    }
+
+    private fun mostrarPregunta(index: Int) {
+        // Lógica para mostrar la pregunta basada en el índice
+        when (index) {
+            0 -> funcionPregunta1()
+            1 -> funcionPregunta2()
+            2 -> funcionPregunta3()
+            3 -> funcionPregunta4()
+            4 -> funcionPregunta5()
+            5 -> funcionPregunta6()
+            else -> {}
+        }
+    }
+    // funcion que te saca del programa
+    private fun salirDeLaAplicacion() {
+        Platform.exit()
     }
 }
